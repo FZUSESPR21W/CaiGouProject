649509717eca2c24397ad01e976268f5bd98e9d5
kirito0206 <445008864@qq.com>
2021-04-26 20:51:40 +0800

    初始化项目

---
+++ caigouapp/app/src/main/java/com/example/caigouapp/ui/mine/MineFragment.java
@@ -0,0 +1,20 @@
+package com.example.caigouapp.ui.mine;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.caigouapp.R;
+
+public class MineFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_mine, container, false);
+        return root;
+    }
+}
